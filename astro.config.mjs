90bd354a81c053b24934854f6f6690aa09140a2d
spichy <Sufursky@gmail.com>
2024-05-06 23:27:57 +0000

    ss

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost:4321",
-
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
 });
