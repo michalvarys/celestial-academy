b5e90e3f245eb03afc1024665a1ae0134829b77c
Michal Varys <Sufursky@gmail.com>
2024-05-07 04:26:34 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { URL, BASE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: URL,
+  base: BASE,
 
   integrations: [mdx(), sitemap(), tailwind()]
 });
